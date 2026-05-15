c728d0c5eaacd1a8d39b3c5c5512cd5ed78d478c
JagadishSunilPednekar <jsphere16@gmail.com>
2026-05-15 19:28:21 +0530

    feat: add plugin configuration files

---
+++ SV-Abid/config/constants.rb
@@ -0,0 +1,19 @@
+module ProjectConstants
+
+    PLUGIN_NAME = 'Abid Building by laws (Bangalore)'
+    PLUGIN_VERSION = '1.0.0'        
+    PLUGIN_CREATOR = 'Sharddha Group of Companies'
+    PLUGIN_CREATOR_EMAIL = 'sharddha@example.com'
+    PLUGIN_DESCRIPTION = 'A custom project plugin for Abid Building by laws (Bangalore)'  
+    PLUGIN_COPYRIGHT = 'Copyright © 2025 Sharddha Group of Companies. All rights reserved.'
+    
+    MESSAGE_HEIGHT_EXCEED = 'Height_Limit_Exceeded'
+    MAX_HEIGHT_MM = 10000  # 10 meters    
+
+    FSI_CONFIG_FILE = 'fsi-config.json',
+    FSI_MESSAGE_VIOLATION = 'FSI violation'                
+
+    SETBACK_CONFIG_FILE = 'setback-config.json'
+    SETBACK_MESSAGE_VIOLATION = 'Setback violation'
+
+  end 
